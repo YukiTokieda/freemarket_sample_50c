8cfeb99743bdf6dd5eb9170ac6eb2ef0e7fae026
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-11 13:27:43 +0900

    Revert "Generate mypage controller"
    This reverts commit 438ff8ca3f4426687de6ca3e6662d0352a2aebd6.

--- test/controllers/mypage_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MypageControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get mypage_index_url
-    assert_response :success
-  end
-
-end
