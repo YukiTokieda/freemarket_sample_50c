c5e431fbfa431c02d092cbcf37867b4362b98297
lsaqrxq <sayuri@MatsubaranoMacBook-ea.local>
2019-05-13 17:51:29 +0900

    Purchase confirmation view 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190513040103) do
+ActiveRecord::Schema.define(version: 20190513053500) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",               default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
