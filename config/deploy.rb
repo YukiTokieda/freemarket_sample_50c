8dc0d464a564c1cf6807ff5d04f18a11a9da0db0
yuki tokieda <tokki7127@icloud.com>
2019-05-13 22:39:32 +0900

    fix Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,26 @@ lock "~> 3.11.0"
 set :application, 'freemarket_sample_50c'
 set :repo_url,  'git@github.com:YukiTokieda/freemarket_sample_50c.git'
 
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/gptyn7127.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
