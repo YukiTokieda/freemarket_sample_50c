3aafe8ab96a479db239a970fe52ccfede94b7dd8
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-13 16:24:27 +0900

    Add columns to profile model

--- db/migrate/20190513070255_create_profiles.rb
+++ db/migrate/20190513070255_create_profiles.rb
@@ -1,7 +1,21 @@
 class CreateProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :profiles do |t|
-      
+      t.text        :body
+      t.string      :first_name
+      t.string      :last_name
+      t.string      :first_name_kana
+      t.string      :last_name_kana
+      t.string      :birth_year
+      t.string      :birth_month
+      t.string      :birth_day
+      t.integer     :phone_number, unique: true
+      t.ineger      :zipcode
+      t.integer     :prefecture
+      t.string      :city
+      t.string      :district
+      t.string      :building
+      t.references  :user, null: false, index: true, foreign_key: true
       t.timestamps
     end
   end
