791c79aa7e3b835b3c418ade0265bfecf1cde80c
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-14 10:17:59 +0900

    Create dummy users data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "csv"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CSV.foreach('db/dummy_datas/users.csv') do |record|
+  User.create(nickname: record[0], email: record[1], password: record[2], password_confirmation: record[3])
+end
