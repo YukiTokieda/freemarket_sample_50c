51ac70e66135ed09512cf901f94504208628d6d6
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-15 11:21:09 +0900

    Refact and add tables data

--- db/migrate/20190514235614_create_brands.rb
+++ db/migrate/20190514235614_create_brands.rb
@@ -1,7 +1,7 @@
 class CreateBrands < ActiveRecord::Migration[5.0]
   def change
     create_table :brands do |t|
-      t.string      :name
+      t.string      :name, index: true
       t.references  :brand_group, null: false, index: true, foreign_key: true
       t.timestamps
     end
