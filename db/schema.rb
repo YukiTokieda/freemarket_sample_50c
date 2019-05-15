5ebfa49a19d286e3872f5768c5169e7d28d789ff
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-15 11:28:09 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,8 @@ ActiveRecord::Schema.define(version: 20190515004820) do
     t.integer  "brand_group_id", null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.index ["brand_group_id"], name: "index_brands_on_brand_group_id", unique: true, using: :btree
+    t.index ["brand_group_id"], name: "index_brands_on_brand_group_id", using: :btree
+    t.index ["name"], name: "index_brands_on_name", using: :btree
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -34,7 +35,6 @@ ActiveRecord::Schema.define(version: 20190515004820) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.index ["grandparent_id"], name: "index_categories_on_grandparent_id", using: :btree
-    t.index ["name"], name: "index_categories_on_name", unique: true, using: :btree
     t.index ["parent_id"], name: "index_categories_on_parent_id", using: :btree
   end
 
