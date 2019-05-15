388875c6165161f32087ea7c94b9363a819b3114
yuki tokieda <tokki7127@icloud.com>
2019-05-15 14:52:04 +0900

    Bash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'TE50c' && password == '5050'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
