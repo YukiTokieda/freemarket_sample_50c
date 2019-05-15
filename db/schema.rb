9a8cca9ffbc7a1695337ae9310aa489b578751b8
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-15 09:26:35 +0900

    Migrate model shipping

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190515001801) do
+ActiveRecord::Schema.define(version: 20190515002155) do
 
   create_table "brand_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20190515001801) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.index ["user_id"], name: "index_profiles_on_user_id", unique: true, using: :btree
+  end
+
+  create_table "shippings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "method",                 null: false
+    t.string   "prefecture_from",        null: false
+    t.string   "period_before_shipping", null: false
+    t.string   "fee_burden",             null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
