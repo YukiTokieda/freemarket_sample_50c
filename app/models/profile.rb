0f618d97d4d6d0f2ebf8429042dc77244ec2672d
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-16 12:30:46 +0900

    Refact validate

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_year, presence: true
-  validates :birth_month, presence: true
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birth_year
+    validates :birth_month
+    validates :birth_day
+  end
 end
