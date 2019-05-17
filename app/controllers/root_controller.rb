bd295fbf71b9a8b673f0280582ea580c921ce728
yuki tokieda <tokki7127@icloud.com>
2019-05-17 15:26:01 +0900

    トップページ

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,15 @@
 class RootController < ApplicationController
-  def index
 
+  def index
+    @ladies= Product.where(category_id: 159..337).limit(4)
+    @mens= Product.where(category_id: 338..467).limit(4)
+    @kids= Product.where(category_id: 468..586)
+    @cosmetics= Product.where(category_id: 886..952)
+
+    @chanel= Product.where(brand: "シャネル")
+    @loisvuitton= Product.where(brand: "ルイヴィトン")
+    @supreme= Product.where(brand: "シュプリーム")
+    @nike= Product.where(brand: "ナイキ")
   end
+
 end
