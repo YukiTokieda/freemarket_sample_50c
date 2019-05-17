1c9459bf36a12d1da0de0634dfacecb60c41e775
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-17 13:28:33 +0900

    delete unnecessary lines

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,9 +19,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_from(provider)
     provider = provider.to_s
-
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
