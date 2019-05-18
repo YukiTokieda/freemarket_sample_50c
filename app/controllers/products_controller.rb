921f9c93451a665929ebf2ca405b4e1a1c05f84d
yu-kimura <yukumura870@gmail.com>
2019-05-18 12:54:31 +0900

    product-delection

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,18 @@
 class ProductsController < ApplicationController
   def index
   end
- 
+
   def new
   end
 
   def show
+    # @product = Product.find(params[:id])
+  end
+
+  def destroy
+    products = Products.find(params[:id])
+      if products.user_id == current_user.id
+        products.destroy
+      end
   end
 end
