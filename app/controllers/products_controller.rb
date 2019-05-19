b111a67dc4a61730dde7932c1c756d905828b685
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-19 12:09:15 +0900

    Develop product edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,41 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, except: :show
   def index
   end
  
   def new
   end
 
+  def create
+  end
+
   def show
+  end
+
+  def edit
+    set_product
+    @categories = Category.where(parent_id: 0)
+    @states = State.all
+  end
+
+  def update
+    set_product
+    if @product.update(update_params)
+      redirect_to controller: :root, action: :index
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def update_params
+    params.require(:product).permit(:name, :description, :category_id, :state_id, :price,
+      shipping_attributes:[:method, :prefecture_from, :period_before_shipping, :fee_burden, :_destroy, :id],
+      images_attributes:[:name, :_destroy, :id])
   end
 end
