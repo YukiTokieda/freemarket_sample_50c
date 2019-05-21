b18e0a86e97f467939d5959ede542aff13a03a67
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-21 15:50:51 +0900

    Create update action for controllers

--- app/controllers/mypage/identification_controller.rb
+++ app/controllers/mypage/identification_controller.rb
@@ -1,6 +1,22 @@
 class Mypage::IdentificationController < ApplicationController
   before_action :authenticate_user!
   def edit
-    @profile = Profile.find_by(user_id: current_user.id)
+    set_identification
+  end
+
+  def update
+    set_identification
+    @identification.update(identification_params)
+    redirect_to action: :edit
+  end
+
+  private
+
+  def set_identification
+    @identification = Profile.find_by(user_id: current_user.id)
+  end
+
+  def identification_params
+    params.require(:profile).permit(:zipcode, :prefecture, :city, :district, :building)
   end
 end
--- app/controllers/mypage/profiles_controller.rb
+++ app/controllers/mypage/profiles_controller.rb
@@ -1,5 +1,23 @@
 class Mypage::ProfilesController < ApplicationController
   before_action :authenticate_user!
-  def index
+  def edit
+    set_profile
   end
+
+  def update
+    set_profile
+    @profile.update(profile_params)
+    redirect_to action: :edit
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find_by(user_id: current_user.id)
+  end
+
+  def profile_params
+    params.require(:profile).permit(:body)
+  end
+
 end
