e61653ace82f0b59c260116fb53bf110e90cc3a0
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-05-23 16:40:28 +0900

    Update 20190521000336_create_orders.rb

--- db/migrate/20190521000336_create_orders.rb
+++ db/migrate/20190521000336_create_orders.rb
@@ -2,10 +2,9 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       t.string     :status, null: false, default: "waiting_for_shipping"
-      t.references  :product, null: false, foreign_key: true
-      t.references  :trading, null: false, foreign_key: true
+      t.references  :product, null: false, index: { unique: true }, foreign_key: true
+      t.references  :trading, null: false, index: { unique: true }, foreign_key: true
       t.timestamps
     end
-    add_index :orders, [:product_id, :trading_id], unique: true
   end
 end
