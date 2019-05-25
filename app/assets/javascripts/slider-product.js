00ea02b9905e6dff373540db4bfea34105fb1fbb
lsaqrxq <sayuri@MatsubaranoMacBook-ea.local>
2019-05-25 14:25:59 +0900

    Create slider product view 2

--- app/assets/javascripts/slider-product.js
+++ app/assets/javascripts/slider-product.js
@@ -1,19 +1,24 @@
 $(document).on('turbolinks:load', function(){
 
-  // ドット作成
+  // dots:true : ドットの描写
   $('.owl-carousel.owl-loaded.owl-drag').slick({
     dots:true,
-    // ページングテンプレート（ドットリストの部分）をカスタマイズ
+    // customPaging : ページングテンプレート（ドットリストの部分）をカスタマイズ
+    // (slick,index) : slickのオブジェクト（コード）と、index(数字)が渡されている
     customPaging: function(slick,index) {
       // スライダーのインデックス番号に対応した画像のsrcを取得
-      var targetImage = $(slick.$slides[index]).attr('src');
+      var targetImage = $(slick.$slides[index]).find('img').attr('src');
+      console.log(targetImage)
       // slick-dots > li の中に上記で取得した画像を設定
       return '<img src=" ' + targetImage + ' "/>';
     }
   });
+
+  //マウスポインターが要素に乗ったらクリックする動きでホバーの動きを作成
   $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseenter', '.slick-dots > li', function() {
     $( this ).click();
   });
+  //CSSの変化をつける
   $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseover', '.slick-dots > li', function() {
     $( this ).css({
       opacity: "1",
