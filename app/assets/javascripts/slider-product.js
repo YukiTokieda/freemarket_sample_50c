b42ff2f9a1219f42323a3c1e80d5e0c0be84fef1
lsaqrxq <sayuri@MatsubaranoMacBook-ea.local>
2019-05-26 16:54:12 +0900

    Create slider product view 3

--- app/assets/javascripts/slider-product.js
+++ app/assets/javascripts/slider-product.js
@@ -1,34 +1,29 @@
 $(document).on('turbolinks:load', function(){
-
-  // dots:true : ドットの描写
   $('.owl-carousel.owl-loaded.owl-drag').slick({
+    arrows: false,
     dots:true,
-    // customPaging : ページングテンプレート（ドットリストの部分）をカスタマイズ
-    // (slick,index) : slickのオブジェクト（コード）と、index(数字)が渡されている
+    dotsClass: 'slide-dots',
     customPaging: function(slick,index) {
-      // スライダーのインデックス番号に対応した画像のsrcを取得
       var targetImage = $(slick.$slides[index]).find('img').attr('src');
-      console.log(targetImage)
-      // slick-dots > li の中に上記で取得した画像を設定
-      return '<img src=" ' + targetImage + ' "/>';
+      return '<img src=" ' + targetImage + ' ", class = "owl-dots" "/>';
     }
   });
 
-  //マウスポインターが要素に乗ったらクリックする動きでホバーの動きを作成
-  $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseenter', '.slick-dots > li', function() {
+  $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseenter', '.slide-dots > li', function() {
     $( this ).click();
   });
-  //CSSの変化をつける
-  $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseover', '.slick-dots > li', function() {
+  $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseover', '.slide-dots > li', function() {
     $( this ).css({
       opacity: "1",
       cursor: "pointer"
     });
   });
-  $( '.owl-carousel.owl-loaded.owl-drag' ).on( 'mouseout', '.slick-dots > li', function() {
-    $( this ).css({
-      opacity: "",
-      cursor: ""
-    });
+
+  $(".owl-dots").hover(
+    function () {
+      $(this).css('opacity', '1.0');
+  },
+    function () {
+      $(this).css('opacity', '0.5');
   });
 });
