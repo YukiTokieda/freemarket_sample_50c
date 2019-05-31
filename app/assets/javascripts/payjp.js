8ff878756b9d678c00bf021bcf09554011b0f60c
yuki tokieda <tokki7127@icloud.com>
2019-05-31 15:35:36 +0900

    Payjp 検証

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -2,7 +2,7 @@
   document.addEventListener(
     "DOMContentLoaded", e => {
       if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-        Payjp.setPublicKey("pk_live_a35d045fa72e23f6565c3ea9"); //ここに公開鍵を直書き
+        Payjp.setPublicKey("pk_test_7aed2182e9b7224c66756ba7"); //ここに公開鍵を直書き
         // TODO: 環境変数をローカルと本番で書き分けたい鍵は変数にしたい
         let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
         btn.addEventListener("click", e => { //ボタンが押されたときに作動します
